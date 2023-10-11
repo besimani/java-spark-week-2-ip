1bc53134abb67e9773ab54fb2177e3e44ff4b64f
Bernard Amaitsa <besimani@gmail.com>
2023-10-11 17:20:40 +0300

    project structure

---
+++ Java-spark-week-2-ip/src/main/java/safaricom/javasparkweek2ip/HelloController.java
@@ -0,0 +1,14 @@
+package ke.co.safaricom.javasparkweek2ip;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
